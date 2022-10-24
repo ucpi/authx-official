0148e19d36977fa086b31751727c12752ad68d8d
Karan <karanchandekar431@gmail.com>
2022-10-24 19:38:29 +0530

    created and styled section1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Section1 from "./Components/Section1/Section1";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Section1 />
     </div>
   );
 }
