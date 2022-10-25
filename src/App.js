5ca10a2f8eff429cd25487bc25b2244440376e9c
Karan <karanchandekar431@gmail.com>
2022-10-25 11:53:15 +0530

    created and styled section2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Section1 from "./Components/Section1/Section1";
+import Section2 from "./Components/Section2/Section2";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Section1 />
+      <Section2 />
     </div>
   );
 }
